0be212869d63c01fbe4b60b95abcb476ca2797b6
DragosPetri <dragospetri@yahoo.com>
2022-11-06 13:36:47 +0200

    added bad request lmao

--- comunicau.py
+++ comunicau.py
@@ -2,6 +2,7 @@ import websockets
 import asyncio
 import json
 from qr import QR
+import time
 from door import simulate_door
 
 path_to_image = "path.txt"
@@ -10,16 +11,26 @@ path_to_image = "path.txt"
 def handle(msg):
     if (msg == "open"):
         simulate_door()
+    if (msg == "unauthorized"):
+        f = open(path_to_image, "w")
+        f.write("bad.png")
+        f.close()
+        time.sleep(3)
+        f = open(path_to_image, "w")
+        f.write("qr.png")
+        f.close()
 
 
 async def listen():
     url = "ws://financeapp.tudoresan.ro:8080/room/ws?id=6366b5b04483e1e9fbd14fc0"
-    async with websockets.connect(url, ping_interval=None) as ws:
+    while (1):
+        async with websockets.connect(url, ping_interval=None) as ws:
 
-        while True:
-            msg = await ws.recv()
-            data = json.loads(msg)
-            handle(data['message'])
+            while True:
+                msg = await ws.recv()
+                data = json.loads(msg)
+                print(data)
+                handle(data['message'])
 
 
 asyncio.run(listen())
